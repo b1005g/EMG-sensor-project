5ee9e460f5e9e8adfc987f2880804c696bd8c95c
b1005g <69081460+b1005g@users.noreply.github.com>
2022-01-22 12:05:37 +0900

    Delete re package.py

--- re package.py
+++
@@ -1,5 +0,0 @@
-import re
-bong = [b'', b'CLEARDATA\r\n', b'LABEL,EMG\r\n', b'DATA,973\r\n', b'DATA,972\r\n', b'DATA,972\r\n', b'DATA,973\r\n', b'DATA,973\r\n', b'DATA,973\r\n', b'DATA,972\r\n', b'DATA,973\r\n', b'DATA,972\r\n', b'DATA,973\r\n', b'DATA,973\r\n', b'DATA,972\r\n', b'DATA,972\r\n', b'DATA,973\r\n', b'DATA,973\r\n', b'DATA,973\r\n', b'DATA,973\r\n',]
-output = re.findall("\d{3}",str(bong))
-print(output)
-
